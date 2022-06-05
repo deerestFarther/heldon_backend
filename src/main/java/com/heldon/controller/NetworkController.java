86ba3d299909d2595407c87544981dc44c6dbf36
deerestFarther <Fred_Bernard_Chuck@protonmail.ch>
2022-06-05 10:33:37 +0800

    Fred committed at 20220605 10:34 with some patches

--- src/main/java/com/heldon/controller/NetworkController.java
+++ src/main/java/com/heldon/controller/NetworkController.java
@@ -39,7 +39,7 @@ public class NetworkController {
     @ApiOperation("按netId删除关系网")
     public Boolean deleteNetworkByNetId(@PathVariable int netId) {
         Map<String, Object> map = new HashMap<>();
-        map.put("netId", netId);
+        map.put("net_Id", netId);
         return networkService.removeByMap(map);
     }
 
@@ -65,13 +65,13 @@ public class NetworkController {
         return networkService.getRootIdNameByNetId(netId);
     }
 
-//    @GetMapping("/getRootIdNameByNetId/{netId}")
-//    @ApiOperation("按netId查找关系网的rootIdName")
-//    public List<Network> getNetworkByUserId(@PathVariable long userId) {
-//        Map<String, Object> map = new HashMap<>();
-//        map.put("user_id", userId);
-//        return networkService.listByMap(map);
-//    }
+    @GetMapping("/getNetworkByUserId/{userId}")
+    @ApiOperation("按userId查找关系网的rootIdName")
+    public List<Network> getNetworkByUserId(@PathVariable long userId) {
+        Map<String, Object> map = new HashMap<>();
+        map.put("user_id", userId);
+        return networkService.listByMap(map);
+    }
 
 
 //    todo 前端请求起来麻烦的话就写
